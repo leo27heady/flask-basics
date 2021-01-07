4bb22ac671ab4c739547ff491b1fff95cd92bb11
Leonid Kulyk <leonidkulik2707@gmail.com>
2021-01-07 20:45:00 +0200

    Image sending form flask-client. Receiving and classifying in flask-sever than send classification result like json to sender.

--- src/flask-client/client.py
+++ src/flask-client/client.py
@@ -1,12 +1,55 @@
-from flask import Flask, render_template
+import os
+import requests
+import json
+
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = os.path.dirname(__file__)
+
+ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg"]
 
 
-@app.route("/")
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    )
+
+
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+
+        filename = file.filename
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+
+        # check filename isn't empty
+        if filename == "":
+            return redirect(request.url)
+
+        # check file is allowed
+        if file and allowed_file(filename):
+            file.save(file_path)
+            url = "http://localhost:5000/uploader"
+            send_image = open(file_path, "rb")
+            files = {"file": send_image}
+
+            r = requests.post(url, files=files)
+            send_image.close()
+            os.remove(file_path)
+
+            pred = json.loads(r.content.decode("utf-8"))
+            return render_template("upload.html") + render_template(
+                "image_class.html", pred=pred
+            )
+
     return render_template("upload.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="localhost", port=5001)
--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -1,6 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import cv2
+
+# from tensorflow import keras
+
+import os
+
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = os.path.dirname(__file__)
+
+model = None
+
+
+def read_image(file_path):
+    image = cv2.imread(file_path)
+    image = cv2.resize(image, (512, 512))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    os.remove(file_path)
+    return image
+
+
+# def load_model(model_path):
+#     return models.load_model(model_path)
 
 
 @app.route("/")
@@ -9,14 +30,22 @@ def index():
 
 
 @app.route("/uploader", methods=["POST"])
-def upload_file():
+def get_file():
     f = request.files["file"]
-    print(type(f))
-    print(len(f))
-    print()
-    f.save(f.filename)
-    return "file uploaded successfully"
+    f.save(os.path.join(app.config["UPLOAD_FOLDER"], f.filename))
+
+    img = read_image(os.path.join(app.config["UPLOAD_FOLDER"], f.filename))
+    print(img.shape)
+    # predict = model.predict(img)
+    print("request url:", request.url)
+    response = {"class_id": 3}
+
+    return jsonify(response)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    model_path = app.config["UPLOAD_FOLDER"] + "\\model\\weights.h5"
+
+    # model = load_model(filename)
+    app.run(debug=True, host="localhost", port=5000)
